cdb1141a54e0bbe519b634081d104e0be290e450
John Tönnäng <john_tonnang@hotmail.com>
2024-04-29 22:09:23 +0200

    Update tailwind config

--- app/tailwind.config.ts
+++ app/tailwind.config.ts
@@ -19,6 +19,23 @@ const config: Config = {
         'main-white': '#fff',
         'main-black': '#000',
         'main-red': '#D92D20',
+        'main-grey': '#667085',
+        'dark-blue': '#101828',
+      },
+      fontSize: {
+        60: '3.75rem',
+        36: '2.25rem',
+        24: '1.5rem',
+        20: '1.25rem',
+        18: '1.125rem',
+        16: '1rem',
+        14: '0.875rem',
+      },
+      lineHeight: {
+        120: '120%',
+      },
+      letterSpacing: {
+        tight: '-1.2px',
       },
     },
   },
