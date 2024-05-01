a7cd80e0311d1d982e9bfcb5f952710432223439
John Tönnäng <john_tonnang@hotmail.com>
2024-05-01 15:30:16 +0200

    Add Menu component to layout instead of page

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,11 +1,9 @@
 import Articles from './components/Articles';
 import Hero from './components/Hero';
-import Menu from './components/Menu';
 
 export default async function Home() {
   return (
     <>
-      <Menu />
       <main className="flex min-h-screen flex-col">
         <Hero />
         <Articles />
