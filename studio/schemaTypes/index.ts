165b958c1474fd8437b65236788285fe70dd808b
John Tönnäng <john_tonnang@hotmail.com>
2024-05-01 15:35:43 +0200

    Add the schemas to schemaTypes

--- studio/schemaTypes/index.ts
+++ studio/schemaTypes/index.ts
@@ -1,7 +1,21 @@
-import {pageType} from './pageType'
+import {heroType} from './heroType'
 import {articleType} from './articleType'
 import {authorType} from './authorType'
 import {linkType} from './linkType'
 import {menuType} from './menuType'
+import {contactType} from './contactType'
+import {articlesType} from './articlesType'
+import {customerType} from './customerType'
+import {customersType} from './customersType'
 
-export const schemaTypes = [pageType, articleType, authorType, linkType, menuType]
+export const schemaTypes = [
+  heroType,
+  articleType,
+  authorType,
+  linkType,
+  menuType,
+  contactType,
+  articlesType,
+  customerType,
+  customersType,
+]
